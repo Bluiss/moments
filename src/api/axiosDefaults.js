845e9e01ab49629f2067ee1545cdea47f27a68ed
Harry Britton <harrydbritton@gmail.com>
2024-05-13 20:12:01 +0000

    update

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://moments-hb-b9a64daf59f3.herokuapp.com";
+axios.defaults.baseURL = "https://moments-hb-api-13c179d1512d.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
